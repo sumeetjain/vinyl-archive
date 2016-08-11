d93b1b7ce33bf341c1d74c3bd05cb4e41ea0d4d6
Colin Narans <cnarans@gmail.com>
2016-08-11 14:27:35 -0500

    albums showing up on artist page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -10,6 +10,7 @@ class ArtistsController < ApplicationController
   # GET /artists/1
   # GET /artists/1.json
   def show
+    @albums = Album.where(:artist_id => @artist.id)
   end
 
   # GET /artists/new
