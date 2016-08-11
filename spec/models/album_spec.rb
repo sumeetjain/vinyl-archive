66f5f99d4382e93676a7884fda346f27bc80dd60
Joshua Salyer <joshsalyer23@gmail.com>
2016-08-11 17:22:11 -0500

    Set up database and root page view, added test

---
+++ spec/models/album_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Album, type: :model do
+	describe "All Albums" do
+		it "Finds complete list of album titles" do
+			visit("/albums")
+			expect(page).to have_content("All Albums")
+		end
+	end
+end
