203aec2877b275397aa40ef8518364156c72a35c
Cory Johnson <cory.r.johnson@cox.net>
2016-08-12 09:10:04 -0500

    Before seeing rootpage, should be reidrected to login/setup page

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   gem 'rspec-rails'
 end
 
+
 group :development do
   gem "quiet_assets"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -12,7 +15,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    resources :user
 
   # Example resource route with options:
   #   resources :products do
