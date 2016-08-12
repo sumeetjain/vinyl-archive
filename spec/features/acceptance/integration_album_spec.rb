addd927565ccfeaf4495342a3ddf7551eee298a1
Joshua Salyer <joshsalyer23@gmail.com>
2016-08-12 15:50:36 -0500

    Added tests for link to album details and back to home page

--- spec/features/acceptance/integration_album_spec.rb
+++ spec/features/acceptance/integration_album_spec.rb
@@ -5,7 +5,15 @@ RSpec.describe Album, type: :model do
 	let!(:album2) { Album.create(name: "Red Rocket", genre: "Blues", release_date: "1999") }
 	let!(:album3) { Album.create(name: "Dance Dance!", genre: "Electronic", release_date: "2011") }
 
+  def visit_album_details(album)
+    visit "/"
+    within "#album_#{album.id}" do
+      click_link "Details"
+    end
+  end
+
   describe "All Albums" do
+
     it "Finds a specific album title" do
       visit("/albums")
 
@@ -23,5 +31,22 @@ RSpec.describe Album, type: :model do
     	expect(album2.release_date).to eq("1999")
     end
 
+  end
+
+  describe "Link to album details" do
+
+    it "Album details show up on details page" do
+      visit_album_details(album)
+
+      expect(page).to have_content(album.name)
+      expect(page).to have_content(album.release_date)
+    end
+
+    it "Link can be clicked to return to home page from details" do
+      visit_album_details(album2)
+      click_link "Home"
+
+      expect(page).to have_content("All Albums")
+    end
   end
 end
