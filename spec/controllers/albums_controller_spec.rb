196706f481cf44d4731d6f8ffa4e09fd808587c5
Joshua Salyer <joshsalyer23@gmail.com>
2016-08-12 09:23:14 -0500

    Added test for albums controller and for new instance creating by index action

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe AlbumsController, type: :controller do
 
+	describe "GET #index" do
+	    it "Returns HTTP success when visiting index" do
+			get :index
+			expect(response).to have_http_status(:success)
+	    end
+
+		it "Creates new instance for all albums" do
+			all_albums = Album.all
+
+			get :index
+			assigns(:albums).should eq(all_albums)
+		end
+	end
+
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Album, type: :model do
 		it "Finds complete list of album titles" do
 			visit("/albums")
 			expect(page).to have_content("All Albums")
+			expect(page).to have_content("Title")
 		end
 	end
 end
