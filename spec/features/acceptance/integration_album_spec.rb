f3ce4f0bf8c4500a921151b2452d2b7348ca162d
Joshua Salyer <joshsalyer23@gmail.com>
2016-08-12 10:17:31 -0500

    Changed path to Album test file, modified Sublime text tab settings, added columns to test database

---
+++ spec/features/acceptance/integration_album_spec.rb
@@ -0,0 +1,11 @@
+require "rails-helper"
+
+RSpec.describe Album, type: :model do
+  describe "All Albums" do
+    it "Finds complete list of album titles" do
+      visit("/albums")
+      expect(page).to have_content("All Albums")
+      expect(page).to have_content("Title")
+    end
+  end
+end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-	describe "All Albums" do
-		it "Finds complete list of album titles" do
-			visit("/albums")
-			expect(page).to have_content("All Albums")
-			expect(page).to have_content("Title")
-		end
-	end
+
 end
+
