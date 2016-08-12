ddb59ef15c5493c7da63878a68151b1adfda69a7
Joshua Salyer <joshsalyer23@gmail.com>
2016-08-12 10:53:36 -0500

    Edited Album feature tests

--- spec/features/acceptance/integration_album_spec.rb
+++ spec/features/acceptance/integration_album_spec.rb
@@ -1,11 +1,27 @@
-require "rails-helper"
+require "rails_helper"
 
 RSpec.describe Album, type: :model do
+	let!(:album) { Album.create(name: "Big Kahuna", genre: "Rock", release_date: "2008") }
+	let!(:album2) { Album.create(name: "Red Rocket", genre: "Blues", release_date: "1999") }
+	let!(:album3) { Album.create(name: "Dance Dance!", genre: "Electronic", release_date: "2011") }
+
   describe "All Albums" do
-    it "Finds complete list of album titles" do
+    it "Finds a specific album title" do
       visit("/albums")
-      expect(page).to have_content("All Albums")
-      expect(page).to have_content("Title")
+
+      expect(page).to have_content("Big Kahuna")
+      expect(album.genre).to eq("Rock")
+      expect(album.name).to eq("Big Kahuna")
     end
+
+    it "Finds multiple album titles" do
+    	all_albums = Album.all
+    	visit "/albums"
+    	
+    	expect(page).to have_content("Dance Dance!")
+    	expect(all_albums.count).to eq(3)
+    	expect(album2.release_date).to eq("1999")
+    end
+
   end
 end
