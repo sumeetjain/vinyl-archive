afadde089850b9fb36f812b2122fdd7f500eaad2
Colin Narans <cnarans@gmail.com>
2016-08-15 13:18:43 -0500

    basic search working

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,12 +2,12 @@ class Search
 	def Search.keywordSearch(query)
 		return_arr = []
 		results = []
-		results.push(PgSearch.multisearch(query))
+		results.push(PgSearch.multisearch(query)[0])
 		results.each do |entry|
 			if entry["searchable_type"] == "Album"
-				return_arr.push(Album.find(entry.searchable_id))
+				return_arr.push(Album.find(entry["searchable_id"]))
 			elsif entry["searchable_type"] == "Artist"
-				return_arr += Album.where(:artist_id=>entry.searchable_id)
+				return_arr += Album.where(:artist_id=>entry["searchable_id"])
 			end
 		end
 
