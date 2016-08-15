a3fe718d8b2d677be7b3e86316ff59f90ede92c5
Cory Johnson <cory.r.johnson@cox.net>
2016-08-15 13:22:54 -0500

    code before generating uploader

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   get 'home/index'
   delete 'users/sign_out'
 
