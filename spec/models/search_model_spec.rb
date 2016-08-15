45d91b9d9292efab4e29c79b2798ede8c7f57040
Colin Narans <cnarans@gmail.com>
2016-08-15 14:43:45 -0500

    simple search test

--- spec/models/search_model_spec.rb
+++ spec/models/search_model_spec.rb
@@ -1,22 +1,16 @@
 require 'rails_helper'
 
-describe 'artists' do
+RSpec.describe Search, type: :model do
 
 	let! (:artist) {Artist.create(:name=>"New Band", :city=>"Fremont", :url=>"fake.jpg", :bio=>"cool guys")}
 	let! (:album) {Album.create(artist_id: artist.id, name: "Big Kahuna", genre: "Rock", release_date: "2008")}
 	let! (:album2) {Album.create(artist_id: artist.id, name: "Red Rocket", genre: "Blues", release_date: "1999")}
 	let! (:album3) {Album.create(artist_id: artist.id, name: "Dance Dance!", genre: "Electronic", release_date: "2011")}
-	
-	it 'displays artist albums' do
-		visit "/artists/#{artist.id}"
+
+	it 'displays all albums for search term' do
+		visit "/search/show?query=Big"
+
 		page.should have_content("Big Kahuna")
-		page.should have_content("Red Rocket")
-		page.should have_content("Dance Dance!")
 	end
 
-	it 'links to album' do
-		visit "/artists/#{artist.id}"
-		click_link 'Big Kahuna'
-		page.should have_content('Big Kahuna')
-	end
 end
