fa55a6899c3432636e87c41d01a66f2804a1e870
Colin Narans <cnarans@gmail.com>
2016-08-16 14:35:36 -0500

    title sort working

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,34 @@
 class Album < ActiveRecord::Base
 	include PgSearch
 	multisearchable :against => [:artist_id, :name]
+
+
+	def Album.getSortedList(albums, sortby)
+		if sortby=="title"
+			return Album.sortTitle(albums)
+		elsif sortby=="artist"
+			return Album.sortArtist(albums)
+		elsif sortby=="date"
+			return Album.sortDate(albums)
+		end
+	end
+
+	def Album.sortTitle(albums)
+		order = {}
+		return_arr = []
+		i=0
+		albums.each do |album|
+			order[album["name"]]=i
+			i+=1
+		end
+
+		order = order.sort
+
+		order.each do |key, value|
+			return_arr.push(albums[value])
+		end
+		return return_arr
+	end
+
+
 end
