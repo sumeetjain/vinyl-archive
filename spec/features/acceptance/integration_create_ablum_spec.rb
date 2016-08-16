37191778fc6ab4427caa66f735c4d6a9cb2f670b
Joshua Salyer <joshsalyer23@gmail.com>
2016-08-15 21:44:41 -0500

    Started adding tests for filling out forms

--- spec/features/acceptance/integration_create_ablum_spec.rb
+++ spec/features/acceptance/integration_create_ablum_spec.rb
@@ -2,19 +2,36 @@ require "rails_helper"
 
 RSpec.describe Album, type: :model do
 
-  def add_an_album(options)
-    :album_name = options[:name]
-    :artist_name = options[:artist_name]
+  def fill_out_album_form(options={})
+    :name = options[:name]
+    :genre = options[:genre]
+    :release_date = options[:release_date]
+    :format = options[:format]
+
     visit "/albums"
     click_link "Add An Album"
-    fill_in "Album Title", with: options[:name]
-    fill_in "Artist Name(s)", with: options[:artist_name]
+    fill_in "Album Title", with: :name
+    fill_in "Genre", with: :genre
+    fill_in "Release Date" with: :release_date
+    fill_in "Format" with: :format
     click_button "Create Album"
   end
 
   describe "Add a new album" do
 
-    it "creates a new album by filling out the form" do
-      add_an_album
+    it "Saves new album details" do
+      options = {name: "Best Beats", genre: "Rock", release_date: Date.new, format: "LP"}
+      # fill_out_album_form(options)
+
+      visit "/albums"
+      click_link "Add An Album"
+      fill_in "Album Title", with: options[:name]
+      fill_in "Genre", with: options[:genre]
+      fill_in "Release Date" with: options[:release_date]
+      fill_in "Format" with: options[:format]
+      click_button "Create Album"
+
+      expect(page).to have_content("Best Beats")
     end
+  end
 end
