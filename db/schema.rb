265a4be5a52523a465ecca897fbd2a0cb57e1c62
Joshua Salyer <joshsalyer23@gmail.com>
2016-08-16 14:37:01 -0500

    Dropped musicians table and re-ran migrations to fix issue with pg_search

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815143957) do
-
+ActiveRecord::Schema.define(version: 20160815185455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +35,6 @@ ActiveRecord::Schema.define(version: 20160815143957) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "musicians", force: :cascade do |t|
     t.integer  "artist_id"
     t.string   "name"
@@ -44,7 +42,6 @@ ActiveRecord::Schema.define(version: 20160815143957) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "pg_search_documents", force: :cascade do |t|
     t.text     "content"
@@ -55,7 +52,6 @@ ActiveRecord::Schema.define(version: 20160815143957) do
   end
 
   add_index "pg_search_documents", ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
-
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
