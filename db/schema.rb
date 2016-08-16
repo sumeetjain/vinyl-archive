a54b08d4ef74517cfa84b14a4ba4e7047eea7cdb
Cory Johnson <cory.r.johnson@cox.net>
2016-08-15 22:30:06 -0500

    File and custom uploaded file name works. Onto album association.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812175834) do
+ActiveRecord::Schema.define(version: 20160815204918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20160812175834) do
     t.string   "city"
     t.text     "url"
     t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "song_file"
+    t.string   "song_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
