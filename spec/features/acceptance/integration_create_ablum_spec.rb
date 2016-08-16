26b444def2f03b1947e6cfe287906630346ed2cf
Joshua Salyer <joshsalyer23@gmail.com>
2016-08-16 12:40:52 -0500

    Having problems with running integration_create_album_spec.rb, expecting keyword end but I don't see where i'm missing one

--- spec/features/acceptance/integration_create_ablum_spec.rb
+++ spec/features/acceptance/integration_create_ablum_spec.rb
@@ -3,17 +3,13 @@ require "rails_helper"
 RSpec.describe Album, type: :model do
 
   def fill_out_album_form(options={})
-    :name = options[:name]
-    :genre = options[:genre]
-    :release_date = options[:release_date]
-    :format = options[:format]
 
     visit "/albums"
     click_link "Add An Album"
-    fill_in "Album Title", with: :name
-    fill_in "Genre", with: :genre
-    fill_in "Release Date" with: :release_date
-    fill_in "Format" with: :format
+    fill_in "Album Title", with: options[:name]
+    fill_in "Genre", with:       options[:genre]
+    fill_in "Release Date" with: options[:release_date]
+    fill_in "Format" with:       options[:format]
     click_button "Create Album"
   end
 
@@ -21,15 +17,7 @@ RSpec.describe Album, type: :model do
 
     it "Saves new album details" do
       options = {name: "Best Beats", genre: "Rock", release_date: Date.new, format: "LP"}
-      # fill_out_album_form(options)
-
-      visit "/albums"
-      click_link "Add An Album"
-      fill_in "Album Title", with: options[:name]
-      fill_in "Genre", with: options[:genre]
-      fill_in "Release Date" with: options[:release_date]
-      fill_in "Format" with: options[:format]
-      click_button "Create Album"
+      fill_out_album_form(options)
 
       expect(page).to have_content("Best Beats")
     end
