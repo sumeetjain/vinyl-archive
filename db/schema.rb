8285512635264409eb96fc62cd6de3454732ef9c
Cory Johnson <cory.r.johnson@cox.net>
2016-08-16 16:03:43 -0500

    Will upload song name, any file, and associated album_id to songs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815204918) do
+ActiveRecord::Schema.define(version: 20160816203936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160815204918) do
     t.string   "song_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "album_id"
   end
 
   create_table "users", force: :cascade do |t|
