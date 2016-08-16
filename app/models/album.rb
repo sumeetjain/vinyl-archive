850c9b48216c40b933a614d5a660e99cbc8a93d5
Sumeet Jain <sumeet@sumeetjain.com>
2016-08-15 20:34:37 -0500

    Clean up the models

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,8 +1,5 @@
 class Album < ActiveRecord::Base
 	belongs_to :artist
-	# has_and_belongs_to_many :musicians
 	has_many :musicians, :through => :artist
-	# belongs_to :musician, :class_name => :artist
 	accepts_nested_attributes_for :artist, :musicians
-	# accepts_nested_attributes_for :musician
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,4 @@
 class Artist < ActiveRecord::Base
 	has_many :albums
 	has_many :musicians
-	# accepts_nested_attributes_for :albums
-	
-
 end
