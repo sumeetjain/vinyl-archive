827addf6069125a190329e96035a968d4cdee2d6
Colin Narans <cnarans@gmail.com>
2016-08-18 11:43:56 -0500

    slightly expanded test

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -32,18 +32,25 @@ RSpec.describe Album, type: :model do
 		page.should have_content('Millenium')
 		page.should have_no_content('Red Rocket')
 		page.should have_no_content('Old School')
+		page.should have_no_content('Later')
+		page.should have_no_content('Dance Dance!')
 
 		click_link 'EP'
 		page.should have_content('Red Rocket')
 		page.should have_content('Later')
 		page.should have_no_content('Big Kahuna')
 		page.should have_no_content('Old School')
+		page.should have_no_content('Millenium')
+		page.should have_no_content('Dance Dance!')
 
 		click_link 'Single'
 		page.should have_content('Dance Dance!')
 		page.should have_content('Old School')
 		page.should have_no_content('Red Rocket')
 		page.should have_no_content('Later')
+		page.should have_no_content('Big Kahuna')
+		page.should have_no_content('Millenium')
+
 	end
   
 end
