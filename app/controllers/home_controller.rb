bb43543a0854dfa0da114a1cb8219a66e61bf866
Cory Johnson <cory.r.johnson@cox.net>
2016-08-18 11:20:59 -0500

    please commit. Copied some working code from sandbox

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,28 @@ class HomeController < ApplicationController
 	before_action :authenticate_user!
 	
 	def index
+		if params.exclude?('city')
+			artist_array = Artist.all
+			@albums = Album.all
 
-		
+		else
+			artist_array = Artist.where("city = ?", params['city'])
+			albums = Array.new
+			artist_array.each_with_index do |record, index|
+				albums[index] = Album.where("artist_id = ?", record['id'])
+			end
+			@albums = albums.flatten
 
+		end
+
+		@needed_array = Array.new
+		artist_array = Artist.all
+		artist_array.each do |stuff|
+			if @needed_array.include? stuff.city
+			else
+				@needed_array.push(stuff.city)
+			end
+		end
 	end
 	
 end
