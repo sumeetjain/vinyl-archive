6a1e1ad6c7530bc12e9099537d7f2b33f928d41f
Nikki <nicleemart@gmail.com>
2016-08-22 11:03:10 -0500

    started js for add album

---
+++ public/js/addAlbum.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function(){
+
+  var add = document.getElementById("addAlbum");
+
+  add.addEventListener("click", function(){
+
+  	var modal = document.getElementById("modal");
+
+  	modal.style.display = "flex";
+
+  	modal.style.display = "none";
+  });
+
+});
