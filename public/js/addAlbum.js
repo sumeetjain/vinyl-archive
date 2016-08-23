9700fb4b936dc03b76bc36ec2ee452c5e2313677
Nikki <nicleemart@gmail.com>
2016-08-22 20:15:49 -0500

    wip

--- public/js/addAlbum.js
+++ public/js/addAlbum.js
@@ -1,20 +1,10 @@
 window.addEventListener("load", function(){
 
+  var modal = document.getElementById("modal");
+  var modalWrapper = document.getElementById("modal-wrapper");
   var add = document.getElementById("addAlbum");
 
   add.addEventListener("click", function(){
-
-      var modal = document.getElementById("modal");
-  var modalWrapper = document.getElementById("modal-wrapper");
-  /**
- * Like jQuery's slideDown function - uses CSS3 transitions
- * @param  {Node} elem Element to show and hide
- */
-function slideDown(modalWrapper) {
-  modalWrapper.style.maxHeight = "1000px";
-  // We're using a timer to set opacity = 0 because setting max-height = 0 doesn't (completely) hide the element.
-  modalWrapper.style.opacity   = "1";
-}
 
   	modal.style.display = "flex";
     modalWrapper.style.display = "flex";
