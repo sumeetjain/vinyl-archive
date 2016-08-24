dc94173ad901b033fd133a715285029b09b58326
Nikki <nicleemart@gmail.com>
2016-08-24 13:17:50 -0500

    wip

--- public/js/addAlbum.js
+++ public/js/addAlbum.js
@@ -1,19 +1,16 @@
 window.addEventListener("load", function(){
 
-  var modal = document.getElementById("modal");
-  var modalWrapper = document.getElementById("modal-wrapper");
+  var modalWrapper = document.getElementsByClassName("modal-wrapper");
   var add = document.getElementById("addAlbum");
 
   add.addEventListener("click", function(){
 
-  	modal.style.display = "flex";
     modalWrapper.style.display = "flex";
 
     var close = document.getElementById("close");
 
     close.addEventListener("click", function(){
 
-    modal.style.display = "none";
     modalWrapper.style.display = "none";
 
     });
