d64e4eaba9c3ad20c7b765dffc5b534ac7495083
Sumeet Jain <sumeet@sumeetjain.com>
2016-08-26 10:13:28 -0500

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -35,11 +35,6 @@ ActiveRecord::Schema.define(version: 20160819021017) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "age"
-  end
-
   create_table "musicians", force: :cascade do |t|
     t.integer  "artist_id"
     t.string   "name"
@@ -90,4 +85,5 @@ ActiveRecord::Schema.define(version: 20160819021017) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
 end
