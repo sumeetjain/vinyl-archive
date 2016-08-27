fcde85190906dc4d2b161725135782a3537c39d6
Rachel Lyons <rlyons21@hotmail.com>
2016-08-27 10:11:33 -0500

    artist page - WIP

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,6 +6,10 @@ class ArtistsController < ApplicationController
 	def show
 		@artist = Artist.find(params[:id])
 		@albums = Album.where(:artist_id => @artist.id)
+
+		@array_of_cities = Album.list_of_locations
+		@hash_of_city_albums_count = Album.count_of_albums_per_city
+		@genres = Album.uniq.pluck(:genre)
 	end
 
 	private
