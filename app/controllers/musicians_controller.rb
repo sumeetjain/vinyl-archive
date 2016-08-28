d9b9a781f1b4804e55e309799181d07a56bd3597
Nikki <nicleemart@gmail.com>
2016-08-28 15:28:54 -0500

    city controller error

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -8,4 +8,10 @@ class MusiciansController < ApplicationController
 		@albums = Album.where(:artist_id => @musician.artist.id)
 		@img = Artist.find(@musician.artist_id).url
 	end
+			@array_of_cities = Album.list_of_locations
+
+		@hash_of_city_albums_count = Album.count_of_albums_per_city
+		# @genre_album_count = Album.count_of_albums_per_genre
+
+	
 end
