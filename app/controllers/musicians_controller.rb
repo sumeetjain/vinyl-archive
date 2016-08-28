615695e5b61aa79f1e834d7500bad07f3bb70da7
Rachel Lyons <rlyons21@hotmail.com>
2016-08-28 15:33:29 -0500

    updated musicians controller to have vaiables used for sidebar filters

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -7,10 +7,13 @@ class MusiciansController < ApplicationController
 		@musician = Musician.find(params[:id])
 		@albums = Album.where(:artist_id => @musician.artist.id)
 		@img = Artist.find(@musician.artist_id).url
-	end
-			@array_of_cities = Album.list_of_locations
+
+		@array_of_cities = Album.list_of_locations
 
 		@hash_of_city_albums_count = Album.count_of_albums_per_city
+		@genres = Album.uniq.pluck(:genre)
+	end
+	
 		# @genre_album_count = Album.count_of_albums_per_genre
 
 	
