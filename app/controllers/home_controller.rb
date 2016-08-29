12c821981c746857345f7a97fd9b6659efd0d2ee
Nikki <nicleemart@gmail.com>
2016-08-29 11:24:15 -0500

    undid changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -26,52 +26,5 @@ class HomeController < ApplicationController
 		# @genre_album_count = Album.count_of_albums_per_genre
 
 	end
-
-		if params[:genre]
-			@albums = Album.genres(genre: params[:genre])
-			@albums = @albums.page(params[:page]).per(3)
-		else
-			@albums = Album.all.page(params[:page]).per(3)
-		end
-
-		if params[:sort].nil?
-			@albums = Album.all.page(params[:page]).per(3)
-		else
-			@albums = Album.filter(Album.all, params[:sort]).page(params[:page]).per(3)
-		end
-	end
-
-	def show
-
-		@album = Album.find(params[:id])
-		@photo = Photo.where(:album_id => params[:id])
-		@songs = Song.where(:album_id => params[:id])
-		@date = Date.parse(@album.release_date.to_s).strftime('%B %-d, %Y')
-
-	end
-
-	def new
-		@album = Album.new
-		@album.build_artist
-
-		3.times do
-			@album.artist.musicians.build
-		end
-	end
-
-	def create
-		@album = Album.new(album_params)
-
-		if @album.save
-			
-			redirect_to album_path(@album.id)
-		else
-			render :new
-		end
-	end
-
-	private
-	def album_params
-		params.require(:album).permit(:name, :genre, :release_date, :format,
-																 artist_attributes: [:name, :city, :url, :bio, musicians_attributes: [:name, :instrument]])
-	end
+	
+end
