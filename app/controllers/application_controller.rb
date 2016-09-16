e1274d7b0faa45b01c384629632cd4002139b473
Sumeet Jain <sumeet@sumeetjain.com>
2016-09-16 09:34:29 -0500

    Move new album form into layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
+
+  before_action :initialize_new_album
+
+  def initialize_new_album
+    @new_album = Album.new
+    @new_album.build_artist
+
+    3.times do
+      @new_album.artist.musicians.build
+    end
+  end
 end
