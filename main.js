28faa2686a52b0f48ac8cbc49ce422020d8b33a9
Matt Joseph Wurth <mattjwurth@gmail.com>
2017-10-04 11:30:58 -0500

    fixed header

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-<script>
+// sort bar functionality----------------------------
+
+
 function myFunction() {
     var x = document.getElementById("mySelect");
     var i = x.selectedIndex;
     document.getElementById("demo").innerHTML = x.options[i].text;
 }
-</script>
