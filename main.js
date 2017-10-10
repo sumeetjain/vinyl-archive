c33e85b4e99434d6536443b5df425f079b5e64eb
Matt Joseph Wurth <mattjwurth@gmail.com>
2017-10-10 10:46:16 -0500

    changing branches

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // sort bar functionality----------------------------
 
 
-function myFunction() {
-    var x = document.getElementById("mySelect");
-    var i = x.selectedIndex;
-    document.getElementById("demo").innerHTML = x.options[i].text;
-}
+// function sortfunction() {
+//     var x = document.getElementById("mySelect");
+//     var i = x.selectedIndex;
+//     document.getElementById("demo").innerHTML = x.options[i].text;
+// }
 
 
 //active page link highlight-------------------------
@@ -18,8 +18,43 @@ $( document ).ready(function() {
   };
 });
 
+//mobile filters dropdown----------------------------
 
+function myFunction() {
+    var x = document.getElementById("filters");
+    	if (x.style.display === "none") {
+        	x.style.display = "block";
+    	} 	
+    	else {
+        x.style.display = "none";
+    	}
+}
 
+var $window = $(window);
+$window.on('resize', function (){
+	var x = document.getElementById("filters");
+        if ($window.width() > 655){
+            x.style.display = "block";
+        }
+		else {
+        x.style.display = "none";
+    	}
+    });
+
+//animate vinyl logo--------------------------------
+
+$(document).ready(function()
+{
+    $("#logoAnimate").hover(
+        function()
+        {
+            $(this).attr("src", "images/vinyl.gif");
+        },
+        function()
+        {
+            $(this).attr("src", "images/vinyl.png");
+        });
+});
 
 
 
